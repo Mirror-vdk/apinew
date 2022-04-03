7beedc70929645524980e955def7f7f129817505
edevyatkin <prostogeka93@list.ru>
2022-04-03 16:59:42 +1000

    connect TypeORM.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from "../ormconfig";
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+  imports: [
+    AuthModule,
+    TopPageModule,
+    ProductModule,
+    ReviewModule,
+    TypeOrmModule.forRoot(ormconfig)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
